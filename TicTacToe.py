49cede8ddd3572e014b8566593f1acabc94428f4
DillTheBeast <dillonmaltese@gmail.com>
2022-08-15 18:56:29 -0700

    t

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,15 @@
 xTurn = True
 winner = False
 spacesFull = 0
+rows, columns = (3, 3)
+board = [[0]*columns]*rows
 
 #Printing the board
+while winner is False :
+    if xTurn == True :
+        print("It is X's turn")
+        xTurn == True
+    if xTurn == False :
+        print("It is O's turn")
+        xTurn = True
+    winner = True
