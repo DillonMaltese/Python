fac4a64a4b272f7ecc415df835aa86109e58a3c7
DillTheBeast <dillonmaltese@gmail.com>
2023-03-24 13:16:57 -0400

    t

--- Mouse.py
+++ Mouse.py
@@ -2,8 +2,8 @@ import pyautogui as pag
 import random
 import time
 
-#while True:
-    #x = random.randint(600, 700)
-    #y = random.randint(200, 600)
-pag.moveTo(600,200,0.5)
-    #time.sleep(2)
+while True:
+    x = random.randint(600, 700)
+    y = random.randint(200, 600)
+    pag.moveTo(x,y,0.5)
+    time.sleep(2)
