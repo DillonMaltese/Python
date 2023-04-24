76fcda6fcd5fa1eaa0240466eaca773638df6cdf
DillTheBeast <dillonmaltese@gmail.com>
2023-04-24 09:38:28 -0400

    t

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 #import subprocess
 #subprocess.Popen('C:\\Windows\\System32\\calc.exe')
 
-# Import module
-import wmi
-print('Test')
-# Initializing the wmi constructor
-f = wmi.WMI()
+import time, psutil
+def checkIfProcessRunning(processName):
+        '''Check if there are any running process that contains the given name processName.
+        Iterate over the all the running process'''
+        print('Checking if application is running...')
+        for proc in psutil.process_iter():
+                try:
+                        # Check if process name contains the given name string.
+                        if processName.lower() in proc.name().lower():
+                                return True
+                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                        pass
+        return False;
 
-flag = 0
-# Iterating through all the running processes
-for process in f.Win32_Process():
-    if "chrome.exe" == process.Name:
-        print("Application is Running")
-        flag = 1
-        break
-
-if flag == 0:
-    print("Application is not Running")
+chromeRunning = True
+while chromeRunning:
+    if checkIfProcessRunning('chrome'):
+        print('Instance of Chrome is Running...waiting.')
+        time.sleep(30) # Wait 30 seconds and try again
+        print('Checking again...')
+    else:
+        print('Chrome is not running, good to go.')
+        chromeRunning = False # Sets chromeRunning False to exit loop
