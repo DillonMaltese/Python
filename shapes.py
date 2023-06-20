095c4658d9a8320672a3f1db5a364adea50a02ce
DillTheBeast <dillonmaltese@gmail.com>
2023-06-19 20:53:31 -0400

    Create shapes.py

---
+++ shapes.py
@@ -0,0 +1,21 @@
+#Square
+# for i in range(5):
+#     for x in range(5):
+#         print('x', end=' ')
+#     print('')
+
+#Right Triangle
+# for i in range(5):
+#     for x in range(i+1):
+#         print('x', end=' ')
+#     print(' ')
+
+#Other Right Triangle
+# for i in range(5):
+#     for x in range(5-i):
+#         print('x', end=' ')
+#     print(' ')
+
+#Isosceles
+for i in range(5):
+    
