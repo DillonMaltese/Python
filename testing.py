e8f8720b3f7bd6d691740b86c85dc2696eb446f0
DillTheBeast <dillonmaltese@gmail.com>
2023-07-13 13:29:30 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,16 +1,11 @@
-# rows, cols = (7, 7)
-# print("The o will represent you and the x represents the enemy. Move around to find the hidden items")
-# roomPic = [['_'] * cols]*rows
-# for i in range(len(roomPic)):
-#     for j in range(len(roomPic[i])):
-#         print(roomPic[i][j], end = '')
+import numpy as np
+all_coordinates = np.zeros((0, 2))
 
-# Get the size of the 2D array from the user
-size = int(input("Enter the size of the 2D array: "))
+for y in range(2):
+    for x in range(2):
+        coordinate = np.array([[x,y]])
 
-# Create the 2D array
-array_2d = [[0] * size for _ in range(size)]
-
-# Print the 2D array
-for row in array_2d:
-    print(row)
+        # append
+        all_coordinates = np.vstack((all_coordinates, coordinate))
+        print(coordinate)
+print(all_coordinates)
