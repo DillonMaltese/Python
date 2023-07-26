e9516dbe037d976787278357cfe9d3508d89a2fe
DillTheBeast <dillonmaltese@gmail.com>
2023-07-26 10:55:28 -0400

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,12 @@
+from random import randint
+
+startOrder = [4, 2, 3, 1]
+leftSide = []
+rightSide = []
+leftSide1 = 0
+leftSide2 = 0
+rightSide1 = 0
+rightSide2 = 0
+
+for i in range(len(startOrder)):
+    print('yes')
