f84137e7b275c634ba5d72751a6722bb354d8cd8
DillTheBeast <dillonmaltese@gmail.com>
2023-07-27 10:37:07 -0400

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,8 +1,21 @@
 #Square
 for i in range(5):
     for x in range(5):
-        print('x', end=' ')
+        print('x', end = ' ')
     print('')
 
 
 #Right Triangle
+for i in range(5):
+    for x in range(i+1):
+        print('x', end = ' ')
+    print('')
+
+#Equilateral
+
+for i in range(5):
+    for x in range(i - 1):
+        print('x ', end = '')
+    for y in range(i - 2):
+        print('x ' end = ' ')
+    print('')
