c15cefd2e89ca292777bb2c3afc98fc784163b6e
DillTheBeast <dillonmaltese@gmail.com>
2023-07-27 10:33:18 -0400

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,25 +1,8 @@
 #Square
-# for i in range(5):
-#     for x in range(5):
-#         print('x', end=' ')
-#     print('')
+for i in range(5):
+    for x in range(5):
+        print('x', end=' ')
+    print('')
+
 
 #Right Triangle
-# for i in range(5):
-#     for x in range(i+1):
-#         print('x', end=' ')
-#     print(' ')
-
-#Other Right Triangle
-# for i in range(5):
-#     for x in range(5-i):
-#         print('x', end=' ')
-#     print(' ')
-
-#Isosceles
-for i in range(5):
-    for x in range(5-i):
-        print(' ', end=' ')
-    for x in range(i+1):
-        print('x', end=' ')
-    print('x')
