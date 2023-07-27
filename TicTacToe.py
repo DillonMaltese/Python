585e9c4d84dc6dd1878862d0570973b3c05ae6f5
DillTheBeast <dillonmaltese@gmail.com>
2023-07-27 11:15:09 -0400

    asd

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,59 +1,33 @@
-from array import *
-
-Board = ['/ ', '/ ', '/ '], ['/ ', '/ ', '/ '], ['/ ', '/ ', '/ ']
-winner = False
+board = []
 xTurn = True
+winner = False
+inputX = 0
+inputY = 0
+chose = False
+shape = 'x'
 
-def printBoard(Board):
-    for x in range(3):
-      print(''.join(Board[x]))
+#Adds - to board
+for i in range(3):
+    board.append([])
+    for j in range(3):
+        board[i].append("-")
 
-def takeInput(Board, xTurn):
-  if xTurn == True:
-    rowInput = int(input("What row does x want to go into\n"))
-    colInput = int(input("What column does x want to go into\n"))
-    if(Board[rowInput][colInput] != '/ '):
-      print("You can't do that")
-      takeInput(Board, xTurn)
-    else:
-      Board[rowInput][colInput] = 'x '
-  else:
-    rowInput = int(input("What row does o want to go into\n"))
-    colInput = int(input("What column does o want to go into\n"))
-    if(Board[rowInput][colInput] != '/ '):
-      print("You can't do that")
-      takeInput(Board, xTurn)
-    else:
-      Board[rowInput][colInput] = 'o '
-
-def checkWinner(Board, winner):
-  if Board[0][0] and Board[1][1] and Board[2][2] == 'x ' or Board[0][0] and Board[1][1] and Board[2][2] == 'o ':
-    if Board[0][0] == 'x ':
-      print("X Won!!!")
-      winner = True
-    else:
-      print("Y Won!!!")
-      winner = True
-  elif Board[0][2] and Board[1][1] and Board[2][0] == 'x ' or Board[0][2] and Board[1][1] and Board[2][0] == 'o ':
-    if Board[0][2] == 'x ':
-      print("X Won!!!")
-      winner = True
-    else:
-      print("Y Won!!!")
-      winner = True
-  elif Board[1][0] and Board[1][1] and Board[2][1] == 'x ' or Board[1][0] and Board[1][1] and Board[2][1] == 'o ':
-    if Board[1][1] == 'x ':
-      print("X Won!!!")
-      winner = True
-    else:
-      print("Y Won!!!")
-      winner = True
-
-  else:
-      break
+while not winner:
+    #prints board
+    for i in range(3):
+        print()
+        for j in range(3):
+            print(board[i][j], end = " ")
 
 
-while winner == False:
-  printBoard(Board)
-  takeInput(Board, xTurn)
-  xTurn = not xTurn
+    # for row in board:
+    #     print()
+    #     for space in row:
+    #         print(space, end = " ")
+
+    while not chose:
+        inputX = int(input('Please enter the x coordinate you would like to move to\n'))
+        inputY = int(input('Please enter the y coordinate you would like to move to\n'))
+
+        if inputX <= 3 and inputX >= 1 and inputY <= 3 and inputY >= 1:
+            
--- shapes.py
+++ shapes.py
@@ -1,21 +1,52 @@
 #Square
-for i in range(5):
-    for x in range(5):
-        print('x', end = ' ')
-    print('')
+# for i in range(5):
+#     for x in range(5):
+#         print('x', end = ' ')
+#     print('')
 
 
 #Right Triangle
-for i in range(5):
-    for x in range(i+1):
-        print('x', end = ' ')
-    print('')
+# for i in range(5):
+#     for x in range(i+1):
+#         print('x', end = ' ')
+#     print('')
 
 #Equilateral
+# for i in range(5):
+#     for x in range(5 - i - 1):
+#         print(' ', end = '')
+#     for y in range(i + 1):
+#         print('x ', end = '')
+#     print('')
+
+
+#Diamond
+# for i in range(5):
+#     for x in range(5 - i - 1):
+#         print(' ', end = '')
+#     for y in range(i + 1):
+#         print('x ', end = '')
+#     print('')
+
+# for i in range(5):
+#     for y in range(i + 1):
+#         print(' ', end = '')
+#     for x in range(5 - i - 1):
+#         print('x ', end = '')
+#     print('')
+
+
+#Hollow Diamond
+for i in range(5):
+    for x in range(5 - i - 1):
+        print(' ', end = '')
+    for y in range(i + 1):
+        print('x ', end = '')
+    print('')
 
 for i in range(5):
-    for x in range(i - 1):
+    for y in range(i + 1):
+        print(' ', end = '')
+    for x in range(5 - i - 1):
         print('x ', end = '')
-    for y in range(i - 2):
-        print('x ' end = ' ')
     print('')
