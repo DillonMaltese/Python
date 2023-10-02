17f487810689fc689d8cc418f7367f5d102bac36
DillTheBeast <dillonmaltese@gmail.com>
2023-10-02 10:10:24 -0400

    Update Queue.py

--- DataStructures/Queue.py
+++ DataStructures/Queue.py
@@ -14,11 +14,15 @@ class Queue:
     
     #Adds a number to the back of the queue
     def enqueue(self, input):
+        if self.sizeCount == self.size:
+            raise ValueError("Queue is full. Cannot add more elements.")
         self.queue[self.sizeCount] = input
         sizeCount += 1
     
     #Takes the first element out of the queue and returns it
     def dequeue(self):
+        if self.sizeCount == 0:
+            raise ValueError("Stack is empty. Cannot remove any elements.")
         placeHolder = self.queue[0]
         for i in self.sizeCount - 1:
             self.queue[i] = self.queue[i + 1]
@@ -27,8 +31,12 @@ class Queue:
 
     #Prints the element that will first come off of the queue but doesn't remove it
     def peek(self):
+        if self.sizeCount == 0:
+            return None
         print(self.sizeCount[0])
 
     #Same as dequeue but just returns nothing if the queue is empty
-    def poll():
-        
+    def poll(self):
+        if self.sizeCount == 0:
+            return None
+        print(self.queue[self.sizeCount])
