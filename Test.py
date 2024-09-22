a0e8908664732a974c7bd71ff7ed4477d26bc6e4
dillthebeast <dillonmaltese@gmail.com>
2024-09-22 19:30:41 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,15 +1,5 @@
-from elevenlabs import generate, play, voices
-from elevenlabs import set_api_key
-set_api_key("f9c97646ef9d171b94b4c22fa5c7b9be")
-voices = voices()
+x = [1,2,3],[4,5,6]
 
-print(voices)
-
-audio = generate(
-  text="Hi! My name is Bella, nice to meet you!",
-  voice="OldIndian",
-  model="eleven_monolingual_v1"
-)
-
-play(audio)
-#print(audio)
+for y in range(len(x)):
+  for z in range(len(x[y])):
+    print(x[y][z])
